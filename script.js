4d5c188b1d9902a0e3d6e847bc8fb6d69e821936
Subarna Saha <subarna090@gmail.com>
2016-06-06 17:09:54 +0530

    added random color background

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var guess1;
 var guess2;
 var finished = false;
 var count = 0;
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 function on_load()
 {
     var ran1 = Math.random() * 100;
@@ -14,27 +23,34 @@ function on_load()
             guess2=parseInt(guess1);
             count+=1;
             finished=guessFunction();
-        }  
+        }
+    alert("Game over!!");
+    
 }
 
 function guessFunction()
 {
     if(isNaN(guess2)){
         alert("It is not a number." + "\n\n Please enter a number in the range 1 to 100");
+        document.body.style.backgroundColor = getRandomColor();
         return false;}
     
     if(guess2 < 1 || guess2 > 100){
         alert("The number is not in range");
+        document.body.style.backgroundColor = getRandomColor();
         return false;}
     
     if(guess2 < target){
         alert("The number is too small");
+        document.body.style.backgroundColor = getRandomColor();
         return false;}
     
     if(guess2 > target){
         alert("The number is too large");
+        document.body.style.backgroundColor = getRandomColor();
         return false;}
     
     alert("You got it!!" + "\n The number is "+ target + "\n\n It took you "+ count + " guesses to get the number");
+    document.body.style.backgroundColor = getRandomColor();
     return true;
 }
